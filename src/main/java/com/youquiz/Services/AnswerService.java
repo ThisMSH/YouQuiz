053b20a89108a3b9485714f844b5e2a18a69a51e
El Mahdi S.H. <midoyugi@hotmail.com>
2023-11-22 15:25:03 +0100

    Get all answers with pagination, sorting and search [YQ-21]

--- src/main/java/com/youquiz/Services/AnswerService.java
+++ src/main/java/com/youquiz/Services/AnswerService.java
@@ -1,10 +1,15 @@
 package com.youquiz.Services;
 
+import com.youquiz.DTO.AltDTO.AnswerAltDTO;
 import com.youquiz.Entities.Answer;
 import com.youquiz.Exceptions.ResourceAlreadyExists;
 import com.youquiz.Exceptions.ResourceNotFoundException;
 import com.youquiz.Repositories.AnswerRepository;
+import com.youquiz.Utils.Utilities;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,10 +17,12 @@ import java.util.Optional;
 @Service
 public class AnswerService {
     private final AnswerRepository answerRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public AnswerService(AnswerRepository answerRepository) {
+    public AnswerService(AnswerRepository answerRepository, ModelMapper modelMapper) {
         this.answerRepository = answerRepository;
+        this.modelMapper = modelMapper;
     }
 
     public Answer createAnswer(Answer answer) {
@@ -40,5 +47,25 @@ public class AnswerService {
         answerRepository.deleteById(id);
 
         return 1;
+    }
+
+    public Page<AnswerAltDTO> getAllAnswers(String text, int page, int size, String sortBy, String sortOrder) {
+        Pageable pageable = Utilities.managePagination(page, size, sortBy, sortOrder);
+
+        Page<Answer> answers = answerRepository.findAllByAnswerLikeIgnoreCase("%" + text + "%", pageable);
+
+        Page<AnswerAltDTO> answerDTOs = answers.map(an -> modelMapper.map(an, AnswerAltDTO.class));
+
+        if (!answers.hasContent()) {
+            String message = "";
+            if (text.isBlank()) {
+                message = "No answers found in page " + (page + 1) + ".";
+            } else {
+                message = "No answer matching \"" + text + "\" was found.";
+            }
+            throw new ResourceNotFoundException(message);
+        }
+
+        return answerDTOs;
     }
 }
