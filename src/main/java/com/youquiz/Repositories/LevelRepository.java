3eba9114007ed8796763e615bd2f7aeef5561102
El Mahdi S.H. <midoyugi@hotmail.com>
2023-11-22 15:57:45 +0100

    Get all answers with pagination, sorting and search [YQ-32]

--- src/main/java/com/youquiz/Repositories/LevelRepository.java
+++ src/main/java/com/youquiz/Repositories/LevelRepository.java
@@ -1,11 +1,13 @@
 package com.youquiz.Repositories;
 
 import com.youquiz.Entities.Level;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LevelRepository extends JpaRepository<Level, Long> {
     Boolean existsByTitleIgnoreCase(String title);
-    List<Level> findByTitleLikeIgnoreCase(String title);
+    Page<Level> findAllByTitleLikeIgnoreCase(String title, Pageable pageable);
 }
