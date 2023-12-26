ea00fa6666a6e72bec70600c4b974c5c28dc112d
El Mahdi S.H. <midoyugi@hotmail.com>
2023-12-26 14:58:15 +0100

    Adding generic methods for pagination

--- src/main/java/com/youquiz/utils/Utilities.java
+++ src/main/java/com/youquiz/utils/Utilities.java
@@ -1,13 +1,32 @@
 package com.youquiz.utils;
 
 import com.youquiz.exceptions.ResourceBadRequestException;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.MediaType;
 
-public class Utilities {
-    public static Pageable managePagination(int page, int size, String sortBy, String sortOrder) {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utilities<T, C, ID> {
+    private final ModelMapper modelMapper;
+    private final JpaRepository<C, ID> repository;
+
+    public Utilities(ModelMapper modelMapper, JpaRepository<C, ID> repository) {
+        this.modelMapper = modelMapper;
+        this.repository = repository;
+    }
+
+    public static Pageable managePagination(Map<String, Object> params) {
+        int page = (int) params.get("page");
+        int size = (int) params.get("size");
+        String sortBy = (String) params.get("sortBy");
+        String sortOrder = (String) params.get("sortOrder");
+
         if (!sortOrder.equalsIgnoreCase(Sort.Direction.ASC.name()) && !sortOrder.equalsIgnoreCase(Sort.Direction.DESC.name())) {
             throw new ResourceBadRequestException("Please make sure to choose either ascending or descending order.");
         }
@@ -16,6 +35,24 @@ public class Utilities {
             : Sort.by(sortBy).descending();
 
         return PageRequest.of(page, size, sort);
+    }
+
+    public Page<T> getAllContents(Map<String, Object> params, Class<T> targetClass) {
+        Pageable pageable = managePagination(params);
+
+        Page<C> contents = repository.findAll(pageable);
+
+        return contents.map(content -> modelMapper.map(content, targetClass));
+    }
+
+    public static Map<String, Object> params(int page, int size, String sortBy, String sortOrder) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("page", page);
+        params.put("size", size);
+        params.put("sortBy", sortBy);
+        params.put("sortOrder", sortOrder);
+
+        return params;
     }
 
     public static MediaType getMediaType(String extension) {
