70965669101fcce1a98effcf6bec9acae8569971
El Mahdi S.H. <midoyugi@hotmail.com>
2023-12-28 11:34:06 +0100

    Finalizing all the methods of QuizService

--- src/main/java/com/youquiz/repositories/QuizRepository.java
+++ src/main/java/com/youquiz/repositories/QuizRepository.java
@@ -1,6 +1,12 @@
 package com.youquiz.repositories;
 
 import com.youquiz.entities.Quiz;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizRepository extends JpaRepository<Quiz, Long> {}
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    Page<Quiz> findAllByTitleLikeIgnoreCase(String title, Pageable pageable);
+}
--- src/main/java/com/youquiz/services/QuizService.java
+++ src/main/java/com/youquiz/services/QuizService.java
@@ -3,13 +3,16 @@ package com.youquiz.services;
 import com.youquiz.dto.requestdto.QuizRequestDTO;
 import com.youquiz.dto.responsedto.QuizDTO;
 import com.youquiz.entities.Quiz;
+import com.youquiz.entities.Trainer;
 import com.youquiz.exceptions.ResourceNotFoundException;
 import com.youquiz.repositories.QuizRepository;
 import com.youquiz.repositories.TrainerRepository;
 import com.youquiz.services.interfaces.IQuizService;
+import com.youquiz.utils.Utilities;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -33,26 +36,57 @@ public class QuizService implements IQuizService {
             throw new ResourceNotFoundException("Trainer not found.");
         }
 
-        Quiz quiz = quizRepository.save(request, )
+        Quiz quiz = quizRepository.save(modelMapper.map(request, Quiz.class));
+
+        return modelMapper.map(quiz, QuizDTO.class);
     }
 
     @Override
     public QuizDTO update(QuizRequestDTO request) {
-        return null;
+        Quiz quiz = quizRepository.findById(request.getId())
+            .orElseThrow(() -> new ResourceNotFoundException("Quiz not found."));
+        Trainer trainer = trainerRepository.findById(request.getTrainerId())
+            .orElseThrow(() -> new ResourceNotFoundException("Trainer not found."));
+
+        quiz.setTitle(request.getTitle());
+        quiz.setDuration(request.getDuration());
+        quiz.setSuccessScore(request.getSuccessScore());
+        quiz.setCanSeeAnswers(request.getCanSeeAnswers());
+        quiz.setCanSeeResult(request.getCanSeeResult());
+        quiz.setChances(request.getChances());
+        quiz.setRemark(request.getRemark());
+        quiz.setTrainer(trainer);
+
+        Quiz updatedQuiz = quizRepository.save(quiz);
+
+        return modelMapper.map(updatedQuiz, QuizDTO.class);
     }
 
     @Override
-    public QuizDTO delete(Long aLong) {
-        return null;
+    public QuizDTO delete(Long id) {
+        Quiz quiz = quizRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Quiz not found."));
+
+        quizRepository.deleteById(id);
+
+        return modelMapper.map(quiz, QuizDTO.class);
     }
 
     @Override
-    public QuizDTO get(Long aLong) {
-        return null;
+    public QuizDTO get(Long id) {
+        Quiz quiz = quizRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Quiz not found."));
+
+        return modelMapper.map(quiz, QuizDTO.class);
     }
 
     @Override
     public Page<QuizDTO> getAll(Map<String, Object> params) {
-        return null;
+        String title = (String) params.get("title");
+        Pageable pageable = Utilities.managePagination(params);
+
+        Page<Quiz> quizzes = quizRepository.findAllByTitleLikeIgnoreCase("%" + title + "%", pageable);
+
+        return quizzes.map(quiz -> modelMapper.map(quiz, QuizDTO.class));
     }
 }
